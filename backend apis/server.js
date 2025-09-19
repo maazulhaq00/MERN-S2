17d81829751eff69829e1576388de53078076383
Maaz Ul Haq <maazulhaq00@gmail.com>
2025-09-19 10:35:50 +0500

    express apis

--- backend apis/server.js
+++ backend apis/server.js
@@ -21,61 +21,104 @@ app.get("/", (req, res) => {
 // Category Create APIs
 app.post("/categories", async (req, res) => {
     try {
-        if(!('categoryName' in req.body) || !('categoryDescription' in req.body)){
-            res.status(400).json({success: false, message: "categoryName or categoryDescription missing"})
+        if (!('categoryName' in req.body) || !('categoryDescription' in req.body)) {
+            return res.status(400).json({ success: false, message: "categoryName or categoryDescription missing" })
         }
         // Get the data sent from req
-        const {categoryName, categoryDescription} = req.body
+        const { categoryName, categoryDescription } = req.body
 
-        const categoryExist = await Category.findOne({categoryName})
+        const categoryExist = await Category.findOne({ categoryName })
 
-        if(categoryExist){
-            res.status(409).json({success: false, message: `${categoryName} already exist`})
+        if (categoryExist) {
+            return res.status(409).json({ success: false, message: `${categoryName} already exist` })
         }
 
         // Create a category
-        const category = await Category.create({categoryName,categoryDescription})
+        const category = await Category.create({ categoryName, categoryDescription })
 
         // Send response with a new category data
-        res.status(201).json({success:true, category })
+        res.status(201).json({ success: true, category })
     }
-    catch(err){
-        res.status(500).json({success: false, message: "Internal Server Error"})
+    catch (err) {
+        res.status(500).json({ success: false, message: "Internal Server Error" })
     }
 })
 
 app.get("/categories", async (req, res) => {
-    try{
+    try {
 
-    // mongodb sa data lao
-    const categories = await Category.find()
+        // mongodb sa data lao
+        const categories = await Category.find()
 
-    // response mai data bhejo
-    res.status(200).json({ success: true, categories })
+        // response mai data bhejo
+        res.status(200).json({ success: true, categories })
     }
-    catch(err){
-        res.status(500).json({success: false, message: "Internal Server Error"})
+    catch (err) {
+        res.status(500).json({ success: false, message: "Internal Server Error" })
     }
 
 })
 
 app.get("/categories/:id", async (req, res) => {
-    try{
+    try {
 
-    const categoryId = req.params.id 
+        const categoryId = req.params.id
 
-    // mongodb sa data lao
-    const category = await Category.findById(categoryId)
+        // mongodb sa data lao
+        const category = await Category.findById(categoryId)
 
-    // response mai data bhejo
-    res.status(200).json({ success: true, category })
+        // response mai data bhejo
+        res.status(200).json({ success: true, category })
     }
-    catch(err){
-        res.status(500).json({success: false, message: "Internal Server Error"})
+    catch (err) {
+        res.status(500).json({ success: false, message: "Internal Server Error" })
     }
 
 })
 
+app.put("/categories/:id", async (req, res) => {
+
+    try {
+        const categoryId = req.params.id
+
+        if (!('categoryName' in req.body) || !('categoryDescription' in req.body)) {
+            return res.status(400).json({ success: false, message: "categoryName or categoryDescription missing" })
+        }
+        // Get the data sent from req
+        const { categoryName, categoryDescription } = req.body
+
+        await Category.findByIdAndUpdate(categoryId, {categoryName, categoryDescription})
+
+        const updatedCategory = await Category.findById(categoryId)
+
+        res.status(200).json({success: true, category: updatedCategory})
+
+    }
+    catch (err) {
+        res.status(500).json({ success: false, message: "Internal Server Error" })
+    }
+})
+
+app.delete("/categories/:id", async (req, res) => {
+
+    try {
+        const categoryId = req.params.id
+
+        const existCategory = await Category.exists({_id: categoryId})
+
+        if(existCategory == null){
+            return res.status(404).json({success: false, message: "Category doesnot exist"})
+        }
+
+        await Category.findByIdAndDelete(categoryId)
+
+        res.status(200).json({success: true, message: "Category deleted successfully"})
+
+    }
+    catch (err) {
+        res.status(500).json({ success: false, message: "Internal Server Error" })
+    }
+})
 
 // Runing server
 app.listen(process.env.PORT, () => {
